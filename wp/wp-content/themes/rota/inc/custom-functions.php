5003b9358fb970d7f76e8e58ccb48f2c1bf58764
Micke Hasselqvist <micke.hasselqvist@awave.com>
2019-01-07 09:03:24 +0100

    Modifieringar i sök-queryn pga bugg där det påverkade andra queries på sidan

--- wp/wp-content/themes/rota/inc/custom-functions.php
+++ wp/wp-content/themes/rota/inc/custom-functions.php
@@ -83,34 +83,36 @@ add_action( 'wp', 'check_if_static_page' );
 
 remove_action( 'try_gutenberg_panel', 'wp_try_gutenberg_panel' );
 
-function rota_search_where($where){
-  global $wpdb;
-  if (is_search())
-    $where .= "OR (t.name LIKE '%".get_search_query()."%' AND {$wpdb->posts}.post_status = 'publish')";
-  return $where;
+function rota_search_where($where, $q){
+	global $wpdb;
+	if( ! is_admin() && $q->is_main_query() && $q->is_search()) :
+		$where .= "OR (t.name LIKE '%".get_search_query()."%' AND {$wpdb->posts}.post_status = 'publish')";
+	endif;
+	return $where;
 }
 
-function rota_search_join($join){
-  global $wpdb;
-  if (is_search())
-    $join .= "LEFT JOIN {$wpdb->term_relationships} tr ON {$wpdb->posts}.ID = tr.object_id INNER JOIN {$wpdb->term_taxonomy} tt ON tt.term_taxonomy_id=tr.term_taxonomy_id INNER JOIN {$wpdb->terms} t ON t.term_id = tt.term_id";
-  return $join;
+function rota_search_join($join, $q){
+	global $wpdb;
+	if( ! is_admin() && $q->is_main_query() && $q->is_search()) :
+		$join .= "LEFT JOIN {$wpdb->term_relationships} tr ON {$wpdb->posts}.ID = tr.object_id INNER JOIN {$wpdb->term_taxonomy} tt ON tt.term_taxonomy_id=tr.term_taxonomy_id INNER JOIN {$wpdb->terms} t ON t.term_id = tt.term_id";
+	endif;
+	return $join;
 }
 
 function rota_search_groupby($groupby){
-  global $wpdb;
+	global $wpdb;
 
-  // we need to group on post ID
-  $groupby_id = "{$wpdb->posts}.ID";
-  if(!is_search() || strpos($groupby, $groupby_id) !== false) return $groupby;
+	// we need to group on post ID
+	$groupby_id = "{$wpdb->posts}.ID";
+	if(!is_search() || strpos($groupby, $groupby_id) !== false) return $groupby;
 
-  // groupby was empty, use ours
-  if(!strlen(trim($groupby))) return $groupby_id;
+	// groupby was empty, use ours
+	if(!strlen(trim($groupby))) return $groupby_id;
 
-  // wasn't empty, append ours
-  return $groupby.", ".$groupby_id;
+	// wasn't empty, append ours
+	return $groupby.", ".$groupby_id;
 }
 
-add_filter('posts_where','rota_search_where');
-add_filter('posts_join', 'rota_search_join');
+add_filter('posts_where','rota_search_where', 10, 2);
+add_filter('posts_join', 'rota_search_join', 10, 2);
 add_filter('posts_groupby', 'rota_search_groupby');
